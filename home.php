ac0d18f4937c23ba77f9ae3742fb27015ca19dda
piam <olimpiam098@gmail.com>
2017-11-06 21:24:45 +0100

    home file

---
+++ home.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>test font awesome</title>
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<h1>My homepage</h1>
+</body>
+</html>
